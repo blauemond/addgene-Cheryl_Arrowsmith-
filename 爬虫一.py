8fbc26d48fdaa0f39e82a5cc566cc07d1ae606d8
blauemond <55486748+blauemond@users.noreply.github.com>
2020-06-27 21:03:37 +0800

    Update 爬虫一.py

--- 爬虫一.py
+++ 爬虫一.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from lxml import etree
 
-
+#这里的路径是复制下来的表格标签，改成自己的就行
 with open('D:\BaiduYunDownload\YZ Meng\work\你的综述\papapa.txt') as read_file:
 	ss = read_file.read()
 
@@ -11,4 +11,4 @@ table1 = html1.xpath('//table')
 table2 = etree.tostring(table1[0], encoding='utf-8').decode()
 df = pd.read_html(table2, encoding='utf-8', header=0)[0]
 results = list(df.T.to_dict().values())
-df.to_csv('D:\BaiduYunDownload\YZ Meng\python爬虫\std.csv',index =False)
+df.to_csv('D:\BaiduYunDownload\YZ Meng\python爬虫\std.csv',index =False) #自己的路径
